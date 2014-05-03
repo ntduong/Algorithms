a83ef0dead691bd5bd370c424496ad390f6b29af
Duong Nguyen <ntduong268@gmail.com>
2014-05-03 12:06:31 +0900

    fix indentation

--- contests/hackerrank/hack_nov/punc_corrector.py
+++ contests/hackerrank/hack_nov/punc_corrector.py
@@ -1,37 +1,35 @@
-# Rule-based punctuation correction 
+""" Rule-based punctuation correction: Stupid solution :)). """
 import re
 
 L1 = ['a', 'an', 'the', 'time', 'in', 'at', 'of', 'on', 'above', 'below', 'what', 'where', 'when', 'who', 'become', 'had', 'gotten', 'gone', 'seen', 'been']
 
 def correct(sent):
-	tokens = sent.split()
-	n = len(tokens)
-	start = 0
+    tokens = sent.split()
+    n = len(tokens)
+    start = 0
 
-	while 1:
-		try:
-			i = tokens.index("???", start)
-			if i < n-1:
-				next_w = tokens[i+1]
-				if next_w in L1 or next_w.endswith('ing') or next_w.endswith('ed'):
-					tokens[i] = "it's"
-				else:
-					tokens[i] = "its"
-			else:
-				tokens[i] = "its"
-			start = i+1
-		except ValueError:
-			break
-		
-	return ' '.join(tokens)
-	
+    while 1:
+        try:
+            i = tokens.index("???", start)
+            if i < n-1:
+                next_w = tokens[i+1]
+                if next_w in L1 or next_w.endswith('ing') or next_w.endswith('ed'):
+                    tokens[i] = "it's"
+                else:
+                    tokens[i] = "its"
+            else:
+                tokens[i] = "its"
+            start = i+1
+        except ValueError:
+            break
+        
+    return ' '.join(tokens)
+    
 if __name__ == '__main__':
-	N = int(raw_input())
-	sents = []
-	for _ in xrange(N):
-		sents.append(raw_input().strip())
-	
-	for sent in sents:
-		print correct(sent)
-		
-
+    N = int(raw_input())
+    sents = []
+    for _ in xrange(N):
+        sents.append(raw_input().strip())
+    
+    for sent in sents:
+        print correct(sent)
--- contests/hackerrank/hack_nov/utopian_tree.py
+++ contests/hackerrank/hack_nov/utopian_tree.py
@@ -1,19 +1,19 @@
 """ Utopian Tree
-	Constrains: 1 <= T <= 10 and 0 <= N <= 60 
+    Constrains: 1 <= T <= 10 and 0 <= N <= 60. 
 """
 def go(x, n):
-	i = 1
-	while i <= n:
-		if i % 2 == 1:
-			x *= 2
-		else: 
-			x += 1
-		i += 1
-	return x
+    i = 1
+    while i <= n:
+        if i % 2 == 1:
+            x *= 2
+        else: 
+            x += 1
+        i += 1
+    return x
 
 if __name__ == "__main__":
-	T = int(raw_input())
-	for _ in xrange(T):
-		N = int(raw_input())
-		print go(1, N)
-	
+    T = int(raw_input())
+    for _ in xrange(T):
+        N = int(raw_input())
+        print go(1, N)
+    
