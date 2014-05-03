34223341ac24bb52986c3467c1ec444c5569cd18
Duong Nguyen <ntduong268@gmail.com>
2014-05-03 12:25:59 +0900

    updated KMP algorithm

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,29 +1,43 @@
-//build failure function for pattern
-int *build_fail(char *p){
-  int m = strlen(p);
-  int *fail = new int[m+1];
-  int j = fail[0] = -1;
-  for(int i = 1; i <= m; i++){
-    while(j >= 0 && p[j] != p[i-1])
-      j = fail[j];
-    fail[i] = j++;
-  }
-  return fail;
-}
+/**
+    Simple snippet for KMP algorithm.
+    @Todo: Test thoroughly
+*/
 
-int match(char *t, char *p, int *fail){
-  int n = strlen(t);
-  int m = strlen(p);
-  int count = 0;
-  for(int i = 0, k = 0; i < n; i++){
-    while(k >= 0 && p[k] != t[i])
-      k = fail[k];
-    if(++k >= m){
-      count++; //match at t[i-m+1...i]
-      k = fail[k];
+#include<iostream>
+#include<string>
+
+// Build failure function for pattern
+int *build_fail(const string& p) {
+    size_t m = p.length();
+    int *fail = new int[m+1];
+    int j = fail[0] = -1;
+    for(int i = 1; i <= m; i++){
+        while(j >= 0 && p[j] != p[i-1]) j = fail[j];
+        fail[i] = j++;
     }
-  }
-  return count;
+    return fail;
 }
 
+/**
+    Matching pattern against text in O(n+m) time.
+    Return the number of matchs
+*/
+int match(const string& t, const string& p, int *fail){
 
+    size_t n = t.length();
+    size_t m = p.length();
+    int cnt = 0;
+
+    for(int i = 0, k = 0; i < n; i++){
+        while(k >= 0 && p[k] != t[i]) k = fail[k];
+        if(++k >= m){
+            ++cnt; // found match at t[i-m+1 : i]
+            k = fail[k];
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    return 0;
+}
